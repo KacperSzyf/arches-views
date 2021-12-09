bb9d5a0714474bf01e38774be7836fe2dfb9c38b
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-09 11:35:11 +0000

    Add files via upload

--- dashboard_data.py
+++ dashboard_data.py
@@ -1,17 +1,43 @@
 
 from django.http.response import JsonResponse
 
-from ..models.resource import Resource
-from ..models.graph import Graph
+from arches.app.models.system_settings import settings
+from arches.app.models.resource import Resource
+from arches.app.models.graph import Graph
+from arches.app.models.tile import Tile
+
+#isresource is True, resource = branch
+#isresource is False, resource = model
 
 def dashboard_data(request):
+    model_array = []
+    branches_array = []
+    total_model_resources = 0
 
-    data_array = []
+    resources = Resource.objects.exclude()
+    graphs = Graph.objects.exclude(graphid = settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID)
 
-    for gr in Graph.objects.all():
-        temp = {}
-        temp['modelName'] = gr.name
-        temp['totalResources'] = len(Resource.objects.filter(graph = gr))
-        data_array.append(temp)
+    for gr in graphs.exclude(isresource = False):
+        model = {}
+        model['modelName'] = gr.name
+        model['totalResources'] = len(resources.filter(graph = gr))
+        total_model_resources+=model['totalResources']
+        model_array.append(model)
+    
+    for gr in graphs.exclude(isresource = True):
+        branch = {}
+        branch['branchName'] = gr.name
+        branches_array.append(branch)
+    
+    metaData = {'totalResources' : total_model_resources,
+    'totalModels' : len(model_array),
+    'totalBranches' : len(branches_array)}
 
-    return JsonResponse(data_array, safe=False)
+    return JsonResponse(
+        {
+        'metaData' : metaData,
+        'modelCounts': model_array,
+        'branchCounts': branches_array  
+        },
+      json_dumps_params = {'indent' : 2}
+      )
