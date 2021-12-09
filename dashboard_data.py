8c9e7f5861e4e57cf47b25786219f03083b19910
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-09 14:50:00 +0000

    Add files via upload

--- dashboard_data.py
+++ dashboard_data.py
@@ -1,4 +1,3 @@
-
 from django.http.response import JsonResponse
 
 from arches.app.models.system_settings import settings
@@ -16,30 +15,41 @@ def dashboard_data(request):
     model_array = []
     total_model_resources = 0
     concepts = Concept.objects.all()
+
     #Get QuerySet of all Resources
     resources = Resource.objects.all()
+    
     #Get QuerySet of all Graphs excluding the system_settings.py Graph
     graphs = Graph.objects.exclude(graphid = settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID)
 
     #Gather data for each model
     for gr in graphs.exclude(isresource = False):
-        total_model_resources+=len(resources.filter(graph = gr))
-        model_array.append(dict(modelName = gr.name, totalResources = len(resources.filter(graph = gr))))
+        total_model_resources+=resources.filter(graph = gr).count()
+        model_array.append(dict(modelName = gr.name, numberOfResources = len(resources.filter(graph = gr))))
     
     #Gather data for each branch
     branches_array = [dict(branchName = gr.name) for gr in graphs.exclude(isresource = True)]
 
-    #Create metadata dictionary 
-    metaData = {'totalResources' : total_model_resources,
-    'totalModels' : len(model_array),
-    'totalBranches' : len(branches_array)}
+    #Get nodetypes
+    nodetype_values = Concept.objects.values_list('nodetype', flat = True).distinct()
+    
+    #Gather concepts 
+    concept_array = [dict(conceptName = nodetype, conceptCount = concepts.filter(nodetype = nodetype).count()) for nodetype in nodetype_values]
 
-    #return
-    return JsonResponse(
-        {
+    #Create metadata dictionary 
+    metaData = {
+        'totalResources' : total_model_resources,
+        'totalModels' : len(model_array),
+        'totalBranches' : len(branches_array)
+        }
+
+    #Create return dicitonary
+    response = {
         'metaData' : metaData,
         'modelCounts': model_array,
         'branches': branches_array ,
-        },
-      json_dumps_params = {'indent' : 2}
-      )
+        'referenceData' : concept_array
+        }
+
+    #return
+    return JsonResponse( response, json_dumps_params = {'indent' : 2})
