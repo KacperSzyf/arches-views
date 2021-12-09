b06afda503f2a5963c1f508720508d5e504b5c0a
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-09 12:19:50 +0000

    Add files via upload

--- dashboard_data.py
+++ dashboard_data.py
@@ -11,24 +11,28 @@ from arches.app.models.tile import Tile
 
 def dashboard_data(request):
 
+    #varibles
     model_array = []
     total_model_resources = 0
 
-    resources = Resource.objects.exclude()
+    #Get QuerySet of all Resources
+    resources = Resource.objects.all()
+    #Get QuerySet of all Graphs excluding the system_settings.py Graph
     graphs = Graph.objects.exclude(graphid = settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID)
 
+    #Gather data for each model
     for gr in graphs.exclude(isresource = False):
         total_model_resources+=len(resources.filter(graph = gr))
         model_array.append(dict(modelName = gr.name, totalResources = len(resources.filter(graph = gr))))
     
+    #Gather data for each branch
     branches_array = [dict(branchName = gr.name) for gr in graphs.exclude(isresource = True)]
 
-    print(type(graphs))
-    
     metaData = {'totalResources' : total_model_resources,
     'totalModels' : len(model_array),
     'totalBranches' : len(branches_array)}
 
+    #return
     return JsonResponse(
         {
         'metaData' : metaData,
