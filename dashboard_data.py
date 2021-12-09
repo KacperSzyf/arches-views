4624c877e018227308ffb15b161c22ceceed318b
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-09 12:45:27 +0000

    Add files via upload

--- dashboard_data.py
+++ dashboard_data.py
@@ -4,7 +4,8 @@ from django.http.response import JsonResponse
 from arches.app.models.system_settings import settings
 from arches.app.models.resource import Resource
 from arches.app.models.graph import Graph
-from arches.app.models.tile import Tile
+from arches.app.models.models import Concept
+
 
 #isresource is True, resource = branch
 #isresource is False, resource = model
@@ -14,7 +15,7 @@ def dashboard_data(request):
     #varibles
     model_array = []
     total_model_resources = 0
-
+    concepts = Concept.objects.all()
     #Get QuerySet of all Resources
     resources = Resource.objects.all()
     #Get QuerySet of all Graphs excluding the system_settings.py Graph
@@ -28,6 +29,7 @@ def dashboard_data(request):
     #Gather data for each branch
     branches_array = [dict(branchName = gr.name) for gr in graphs.exclude(isresource = True)]
 
+    #Create metadata dictionary 
     metaData = {'totalResources' : total_model_resources,
     'totalModels' : len(model_array),
     'totalBranches' : len(branches_array)}
@@ -37,7 +39,7 @@ def dashboard_data(request):
         {
         'metaData' : metaData,
         'modelCounts': model_array,
-        'branches': branches_array  
+        'branches': branches_array ,
         },
       json_dumps_params = {'indent' : 2}
       )
