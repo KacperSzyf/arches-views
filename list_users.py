12b935c6f6dbe3acf719f3f1d33e07011cbe48b8
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-10 11:02:52 +0000

    Add files via upload

--- list_users.py
+++ list_users.py
@@ -8,9 +8,9 @@ def list_users(request):
     User = get_user_model()
 
     #Get all users 
-    users = [dict(username = usr.username, email = usr.email) for usr in User.objects.all()]
+    users = [dict(username = usr.username, fullName = f"{usr.first_name} {usr.last_name}", email = usr.email, isStaff = usr.is_staff, isSuperUser = usr.is_superuser) for usr in User.objects.all()]
 
     #Create a response dict
     response = dict(users = users)
-    
+
     return JsonResponse(response, json_dumps_params = {'indent' : 2})
