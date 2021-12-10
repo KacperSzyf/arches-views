e82fa3d3ae5b1bd88ed71b91449e645686d031c6
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2021-12-10 11:58:16 +0000

    Add files via upload

--- list_users.py
+++ list_users.py
@@ -3,12 +3,20 @@ from django.http.response import JsonResponse
 
 from django.contrib.auth import get_user_model
 
+def is_none(string):
+    if string.strip(): 
+        return string
+    else:
+        return None
+    
+
+
 def list_users(request):
     #Load the model 
     User = get_user_model()
 
     #Get all users 
-    users = [dict(username = usr.username, fullName = f"{usr.first_name} {usr.last_name}", email = usr.email, isStaff = usr.is_staff, isSuperUser = usr.is_superuser) for usr in User.objects.all()]
+    users = [dict(username = usr.username, fullName = is_none(f"{usr.first_name} {usr.last_name}"), email = is_none(usr.email), isStaff = usr.is_staff, isSuperUser = usr.is_superuser) for usr in User.objects.all()]
 
     #Create a response dict
     response = dict(users = users)
